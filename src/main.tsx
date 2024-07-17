6c543c53af99f1180df5867177188b8c6efa8605
Kim0426 <706shin1728@naver.com>
2024-07-17 18:28:44 +0900

    chore: queryClient 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,27 @@
 import { FluentProvider, webLightTheme } from '@fluentui/react-components';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      useErrorBoundary: true,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <FluentProvider theme={webLightTheme}>
-    <App />
-    </FluentProvider>
+    <QueryClientProvider client={queryClient}>
+      <FluentProvider theme={webLightTheme}>
+        <App />
+      </FluentProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
